618e8191339218f7336688d8cd46d1b282ba29f1
Liam Haining <12472949+lhain08@users.noreply.github.com>
2022-03-17 13:40:30 -0400

    implemented data manager (#9)

--- src/main.py
+++ src/main.py
@@ -1,14 +1,14 @@
 from dash import Dash, html
 
-from api import connect
+from data_manager import DataManager
 
+dm = DataManager()
 app = Dash(__name__) #, external_stylesheets=[dbc.themes.DARKLY])
 
 try:
-    connect.get_client()
     app.layout = html.P("Connected")
-except:
-    app.layout = html.P("Error")
+except Exception as e:
+    app.layout = html.P('error here')
 
 if __name__ == "__main__":
     app.run_server(host='0.0.0.0', debug=True)
