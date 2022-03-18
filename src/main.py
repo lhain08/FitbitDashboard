259e2df62e59ef1e4e175ab460e50dbc78fbd721
Liam Haining <ljhaining@gmail.com>
2022-03-18 12:40:29 -0400

    added dynamic tabs

--- src/main.py
+++ src/main.py
@@ -1,14 +1,16 @@
+import dash
 from dash import Dash, html
+import dash_bootstrap_components as dbc
 
 from data_manager import DataManager
+from tabbing import DashboardTabs
 
 dm = DataManager()
-app = Dash(__name__) #, external_stylesheets=[dbc.themes.DARKLY])
+app = Dash(__name__, external_stylesheets=[dbc.themes.DARKLY])
 
-try:
-    app.layout = html.P("Connected")
-except Exception as e:
-    app.layout = html.P('error here')
+tabs = DashboardTabs(app)
+
+app.layout = tabs.render()
 
 if __name__ == "__main__":
     app.run_server(host='0.0.0.0', debug=True)
