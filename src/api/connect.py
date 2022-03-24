ae061a8f7cd60fd84432b58c088796c04c0007ba
Liam Haining <ljhaining@gmail.com>
2022-03-24 00:28:49 -0400

    added caching

--- src/api/connect.py
+++ src/api/connect.py
@@ -13,6 +13,21 @@ def read_id_and_secret():
         raise Exception("Bad File")
     return CLIENT_ID, CLIENT_SECRET
 
+def read_tokens():
+    try:
+        tokens = open('api/tokens.txt', 'r')
+        access = tokens.readline().strip()
+        refresh = tokens.readline().strip()
+        tokens.close()
+        return access, refresh
+    except:
+        raise Exception("Tokens do not exist")
+
+def write_tokens(access, refresh):
+    f = open('api/tokens.txt', 'w')
+    f.write(access + "\n" + refresh)
+    f.close()
+
 def get_client():
     # Attempt to read in id and secret
     try:
@@ -20,16 +35,21 @@ def get_client():
     except:
         raise Exception("Could not find or read client id and secret")
 
-    # Create server and run browser authorization
+    # Try to open cached tokens
     try:
-        server = OAuth2Server(CLIENT_ID, CLIENT_SECRET, my_uri="http://0.0.0.0:8080", \
-                                     redirect_uri="http://127.0.0.1:8080/authorize")
-        server.start()
+        ACCESS_TOKEN, REFRESH_TOKEN = read_tokens()
     except:
-        raise Exception("Authorization Failed")
-    # Get tokens and client
-    ACCESS_TOKEN = str(server.fitbit.client.session.token['access_token'])
-    REFRESH_TOKEN = str(server.fitbit.client.session.token['refresh_token'])
+        # Create server and wait to run browser authorization
+        try:
+            server = OAuth2Server(CLIENT_ID, CLIENT_SECRET, my_uri="http://0.0.0.0:8080", \
+                                         redirect_uri="http://127.0.0.1:8080/authorize")
+            server.start()
+        except:
+            raise Exception("Authorization Failed")
+        # Get tokens and client
+        ACCESS_TOKEN = str(server.fitbit.client.session.token['access_token'])
+        REFRESH_TOKEN = str(server.fitbit.client.session.token['refresh_token'])
+        write_tokens(ACCESS_TOKEN, REFRESH_TOKEN)
 
     CLIENT = fitbit.Fitbit(CLIENT_ID, CLIENT_SECRET, oauth2=True, access_token=ACCESS_TOKEN, refresh_token=REFRESH_TOKEN,\
                        redirect_uri="http://127.0.0.1:8080/authorize")
