34172909d1570a4c8e4f4c5582a0ef8be12da0cf
Liam Haining <ljhaining@gmail.com>
2022-04-04 17:40:09 -0400

    updated modal with new inputs, set up widgets and data manager to accepts start and end date instead of time period

--- src/dashboard/widgets/widget_interface.py
+++ src/dashboard/widgets/widget_interface.py
@@ -1,10 +1,13 @@
 
 
 class WidgetInterface():
-    def __init__(self, data_manager, data_type, time_period):
+    def __init__(self, data_manager, data_type, start_date, end_date):
         self.data_manager = data_manager
         self.data_type = data_type
-        self.time_period = time_period
+        self.start_date = start_date
+        self.end_date = end_date
+        if self.start_date == self.end_date:
+            self.end_date = '15min' # Set the end date as a period instead for intraday data
 
     def render(self):
         raise Exception("Not Implemented")
