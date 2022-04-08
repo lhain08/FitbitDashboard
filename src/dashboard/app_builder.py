e219d713f4314edf3fe5c0fe4d1a6216f29a01e9
Kullas233 <Kullad@rpi.edu>
2022-04-08 03:44:16 -0400

    app_builder formatting

--- src/dashboard/app_builder.py
+++ src/dashboard/app_builder.py
@@ -8,15 +8,12 @@ from .dashboard_modal import DashboardModal
 
 class AppBuilder:
     """Factory for generating the full app"""
-
     def __init__(self, app, data_manager):
         self.app = app
         self.data_manager = data_manager
         self.tabs = DashboardTabs(self.app)
-
         self.dashmodal = DashboardModal("dashmodal", Input("new-dashboard", "n_clicks"), self.tabs, self.app, self.data_manager)
         self.modal = WidgetModal("modal", Input("new-widget", "n_clicks"), self.tabs, self.app, self.data_manager)
-
         self.navbar = Navbar(self.app, self.tabs, self.modal, self.dashmodal)
 
     def build(self):
