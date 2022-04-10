942881ea75fe1ae46663980b3b200b12f7ca095a
Liam Haining <ljhaining@gmail.com>
2022-04-09 20:28:04 -0400

    autoformatting

--- src/dashboard/widgets/bar_chart.py
+++ src/dashboard/widgets/bar_chart.py
@@ -8,7 +8,9 @@ from .widget_interface import WidgetInterface
 
 
 class BarChartWidget(WidgetInterface):
-    def __init__(self, data_manager, data_type, start_date, end_date, name, goal, trends):
+    def __init__(
+        self, data_manager, data_type, start_date, end_date, name, goal, trends
+    ):
         super().__init__(data_manager, data_type, start_date, end_date, name, goal)
         self.trends = trends
 
@@ -19,9 +21,9 @@ class BarChartWidget(WidgetInterface):
         )
         y = data[self.data_type]
         if self.intraday:
-            x = [datetime.datetime.strptime(d, "%H:%M:%S") for d in data['Time']]
+            x = [datetime.datetime.strptime(d, "%H:%M:%S") for d in data["Time"]]
         else:
-            x = [datetime.datetime.strptime(d, "%Y-%m-%d") for d in data['Time']]
+            x = [datetime.datetime.strptime(d, "%Y-%m-%d") for d in data["Time"]]
 
         # Create the chart
         fig = go.Figure([go.Bar(x=x, y=y)])
@@ -29,14 +31,24 @@ class BarChartWidget(WidgetInterface):
         # Bar charts do not support trendlines so we will get the trend data from a
         # helper figure and copy it over onto our original plot
         if self.trends is not None:
-            if self.trends == 'Linear':
-                scat = px.scatter(x=x, y=y, trendline="ols", trendline_scope="overall",
-                                  trendline_color_override='red')
+            if self.trends == "Linear":
+                scat = px.scatter(
+                    x=x,
+                    y=y,
+                    trendline="ols",
+                    trendline_scope="overall",
+                    trendline_color_override="red",
+                )
             else:
-                scat = px.scatter(x=x, y=y, trendline='rolling', trendline_options={'window':int(len(x)/8)},
-                                  trendline_color_override='red')
-            x_trend = scat["data"][1]['x']
-            y_trend = scat["data"][1]['y']
+                scat = px.scatter(
+                    x=x,
+                    y=y,
+                    trendline="rolling",
+                    trendline_options={"window": int(len(x) / 8)},
+                    trendline_color_override="red",
+                )
+            x_trend = scat["data"][1]["x"]
+            y_trend = scat["data"][1]["y"]
             fig.add_trace(go.Scatter(x=x_trend, y=y_trend))
 
         # To be used in the future
