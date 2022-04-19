3b9b8dac0d752bdcedfef85132fc51b56964de71
Liam Haining <ljhaining@gmail.com>
2022-04-19 11:20:49 -0400

    updated navbar

--- src/dashboard/navbar.py
+++ src/dashboard/navbar.py
@@ -1,25 +1,77 @@
 import dash_bootstrap_components as dbc
-
+from dash import html
+from dash import Input, Output, State
 
 class Navbar:
     def __init__(self, app, tabs, modal, dashmodal, navid="navbar"):
         self.navid = navid
         self.tabs = tabs
-        self.navbar = dbc.NavbarSimple(
-            id=self.navid,
-            children=[
-                dbc.NavItem(dbc.Button("New Widget", id="new-widget", n_clicks=0)),
-                dbc.NavItem(
-                    dbc.Button("New Dashboard", id="new-dashboard", n_clicks=0)
-                ),
-                modal.render(),
-                dashmodal.render(),
-            ],
-            brand="Fitbit Dashboard",
-            brand_href="#",
+        # self.navbar = dbc.NavbarSimple(
+        #     id=self.navid,
+        #     children=[
+        #         dbc.NavItem(dbc.Button("New Widget", id="new-widget", n_clicks=0)),
+        #         dbc.NavItem(
+        #             dbc.Button("New Dashboard", id="new-dashboard", n_clicks=0)
+        #         ),
+        #         modal.render(),
+        #         dashmodal.render(),
+        #     ],
+        #     brand="Fitbit Dashboard",
+        #     brand_href="#",
+        #     color="primary",
+        #     dark=True,
+        # )
+
+        self.navbar = dbc.Navbar(
+            dbc.Container(
+                [
+                    html.A(
+                        # Use row and col to control vertical alignment of logo / brand
+                        dbc.Row(
+                            [
+                                # Our logo goes here
+                                # dbc.Col(html.Img(src=PLOTLY_LOGO, height="30px")),
+                                dbc.Col(dbc.NavbarBrand("Fitbit Dashboard", className="ms-2")),
+                            ],
+                            align="center",
+                            className="g-0",
+                        ),
+                        href="#",
+                        style={"textDecoration": "none"},
+                    ),
+                    dbc.NavbarToggler(id="navbar-toggler", n_clicks=0),
+                    dbc.Collapse(
+                        dbc.Row(
+                            [
+                                dbc.Col(dbc.Button("New Widget", id="new-widget", n_clicks=0)),
+                                dbc.Col(dbc.Button("New Dashboard", id="new-dashboard", n_clicks=0)),
+                                modal.render(),
+                                dashmodal.render(),
+                            ],
+                            className="g-0 ms-auto flex-nowrap mt-3 mt-md-0",
+                            align="center"
+                        ),
+                        id="navbar-collapse",
+                        is_open=False,
+                        navbar=True,
+                    ),
+                ]
+            ),
             color="primary",
             dark=True,
+            id=self.navid,
         )
+
+        # add callback for toggling the collapse on small screens
+        @app.callback(
+            Output("navbar-collapse", "is_open"),
+            [Input("navbar-toggler", "n_clicks")],
+            [State("navbar-collapse", "is_open")],
+        )
+        def toggle_navbar_collapse(n, is_open):
+            if n:
+                return not is_open
+            return is_open
 
     def render(self):
         return self.navbar
